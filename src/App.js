c128d1b889b0a1e43d1e1c81651c570c9f567d90
Asa Ayers <Asa@AsaAyers.com>
2018-11-02 23:30:11 -0700

    Use a WeakMap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import React, { useState } from "react";
 import Form from "./Form";
 import "./App.css";
 
+const currentFunction = new WeakMap()
+// This needs to return the same function every time it is called, but that one
+// unchanging function should call the last `fn` that was passed to this.
+function useWrapperFunction(fn) {
+  const [ anchor ] = useState({})
+
+  if (!currentFunction.has(anchor)) {
+    function wrapperFunction (...args) {
+      const fn = currentFunction.get(wrapperFunction)
+      return fn.apply(this, args)
+    }
+    currentFunction.set(anchor, wrapperFunction)
+  }
+
+  const wrapperFunction = currentFunction.get(anchor)
+  currentFunction.set(wrapperFunction, fn)
+  return wrapperFunction
+}
+
 export default () => {
   const [todos, setTodos] = useState([]);
 
@@ -19,10 +38,12 @@ export default () => {
       )
     );
 
+  const onSubmit = useWrapperFunction(text => setTodos([{ text, complete: false }, ...todos]))
+
   return (
     <div className="App">
       <Form
-        onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+        onSubmit={onSubmit}
       />
       <div>
         {todos.map(({ text, complete }, i) => (
